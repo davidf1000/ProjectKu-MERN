3601330f49583ad02403552978ec2bd6272d80ff
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-02 12:31:28 +0700

    user routing and db schema

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// @route     GET api/auth
+// @route     GET api/post
 // @desc       Test route
 // @access      Public
 
-router.get('/',(req,res)=>res.send('auth route'));
+router.get('/',(req,res)=>res.send('post route'));
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express= require('express');
-const app= express();
-const PORT= 3000 || process.env.PORT;
+const express = require('express');
+const app = express();
+const PORT = 3000 || process.env.PORT;
 
 //DB
 const connectDB = require('./config/db');
 connectDB();
 
+app.get('/', (req, res) => {
+  res.send('Home Route');
+});
 
+app.use('/api/users',require('./routes/api/users') );
+app.use('/api/auth',require('./routes/api/auth') );
+app.use('/api/post',require('./routes/api/post') );
+app.use('/api/profile',require('./routes/api/profile') );
 
-app.get('/',(req,res)=>
-{
-  res.send("Home Route");
-})
-
-app.listen(PORT,()=>
-{
-  console.log("Server Started on port %s",PORT);
-})
-
+app.listen(PORT, () => {
+  console.log('Server Started on port %s', PORT);
+});
