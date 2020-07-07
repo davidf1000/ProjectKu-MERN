ab2ce1fd17841e464b47d6df1e7f5ccbe3ce784d
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-07 18:14:05 +0700

    Add Component : Navbar,Landing,Login,Register

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
-    return (
-        <section class="landing">
+  return (
+    <section class="landing">
       <div class="dark-overlay">
         <div class="landing-inner">
           <h1 class="x-large">Developer Connector</h1>
@@ -11,13 +12,17 @@ export const Landing = () => {
             other developers
           </p>
           <div class="buttons">
-            <a href="register.html" class="btn btn-primary">Sign Up</a>
-            <a href="login.html" class="btn btn-light">Login</a>
+            <Link to="/register" class="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" class="btn btn-light">
+              Login
+            </Link>
           </div>
         </div>
       </div>
     </section>
-    )
-}
+  );
+};
 
 export default Landing;
