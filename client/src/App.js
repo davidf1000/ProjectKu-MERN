1378c6a568c3b5b86e4c72f91275230d1b1862b7
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-07 16:50:46 +0700

    start react dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {Fragment} from 'react';
+import './App.css'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Routes from './components/routing/Routes';
-
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
-
-import './App.css';
 
 const App = () => {
-  useEffect(() => {
-    setAuthToken(localStorage.token);
-    store.dispatch(loadUser());
-  }, []);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route component={Routes} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
+    <Fragment>
+    <Navbar />
+    <Landing />
+    </Fragment>
   );
 };
 
