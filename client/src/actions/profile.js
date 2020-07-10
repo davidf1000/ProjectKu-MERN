29c6a008b235940e2818df0692a56e979a8c65d1
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-10 20:15:50 +0700

    create profile action done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,32 @@ export const getCurrentProfile = () => async dispatch => {
     }
 }
 //status: err.respose.status
+//Create or update profile
+export const createProfile = (formData,history,edit=false)=> async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile',JSON.stringify(formData),config);
+        dispatch({
+            type:Action.GET_PROFILE,
+            payload:res.data
+        });
+        dispatch(setAlert(edit?'Profile Updated':'Profile Created','success'));
+        if(!edit)
+        {
+        history.push('/dashboard');
+        }
+    } catch (err) {
+        const errors= err.response.data.errors;
+        if (errors) {
+            errors.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
+          }
+        dispatch({
+            type:Action.PROFILE_ERROR,
+            payload: {msg:err.response.statusText}
+        });
+    }
+}
