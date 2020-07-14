0c32e48aad89ac516c2034cd100a54ebbe040fb2
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-07-15 00:26:20 +0700

    push heroku test

---
+++ config/production.js
@@ -0,0 +1,6 @@
+{
+    "MongoURI":"mongodb+srv://david:123@cluster0.evap0.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    "jwtSecret":"mytoken",
+    "githubClientId":"98996b519a2f72d18105",
+    "githubSecret": "abae3a46ec2d8ff347f5063fd5a3233ad1eb3853"
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 5000 || process.env.PORT;
-
+const path= require('path');
 //DB
 const connectDB = require('./config/db');
 connectDB();
@@ -9,14 +9,25 @@ connectDB();
 //Middle Ware
 app.use(express.json({extended:false}));
 
-app.get('/', (req, res) => {
-  res.send('Home Route');
-});
+// app.get('/', (req, res) => {
+//   res.send('Home Route');
+// });
 
 app.use('/api/users',require('./routes/api/users') );
 app.use('/api/auth',require('./routes/api/auth') );
 app.use('/api/post',require('./routes/api/post') );
 app.use('/api/profile',require('./routes/api/profile') );
+
+//serve statics assets in production
+if(process.env.NODE_ENV=== 'production')
+{
+  // stat folder
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log('Server Started on port %s', PORT);
